c8e1663740fec013cffd5454bc359750964ce6b2
Cyrus Jose <cyrus.jose@outlook.com>
2020-06-18 21:48:16 -0700

    Made the hamburger menu work

--- Assets/script/movie.js
+++ Assets/script/movie.js
@@ -1,13 +1,19 @@
-var movies = ['Coraline']
-$(document).ready(function(){
+var movies = ["Coraline"];
+$(document).ready(function () {
+  // Required for nav bar mobile functionality
+  $(".sidenav").sidenav();
+  //Required for carousel
+  $(document).ready(function () {
+    $(".carousel").carousel();
+  });
 
-    var movie = $(this).attr("data-name");
-    var queryURL = "https://www.omdbapi.com/?t=" + movies + "&apikey=trilogy";
+  var movie = $(this).attr("data-name");
+  var queryURL = "https://www.omdbapi.com/?t=" + movies + "&apikey=trilogy";
 
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function(response){
-        $('.display').text(JSON.stringify(response));
-    })
-})
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+  }).then(function (response) {
+    // $('.body-container').text(JSON.stringify(response));
+  });
+});
