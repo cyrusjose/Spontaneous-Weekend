6747d093a0b4e863257492fa39df23ce6d9382ee
kate <kan004@ucr.edu>
2020-06-20 12:01:41 -0700

    fixed lattitude and longitude and link

--- Assets/script/food.js
+++ Assets/script/food.js
@@ -1,8 +1,8 @@
 // global variables
 var apikey = "eee323892b3fbbbde9d9b268625c3602";
 var randomBtn = $(".random-btn");
-var lattitude = "33.669445";
-var longitutde = "-117.823059";
+var lattitude = "";
+var longitutde = "";
 var resName = $(".restaurant-name");
 var resPrice = $(".restaurant-price");
 var resAddress = $(".restaurant-address");
@@ -11,22 +11,30 @@ var imgDiv = $(".imgDiv");
 var callBtn = $(".call-btn");
 var webBtn = $(".web-btn");
 var results = $(".results-container");
-
-// put in necessary info in settings 
-var settings = {
-    "async": true,
-    "crossDomain": true,
-    "url": "https://developers.zomato.com/api/v2.1/search?lat=" + lattitude + "&lon=" + longitutde + "&sort=cost",
-    "method": "GET",
-    "headers": {
-        "user-key": "eee323892b3fbbbde9d9b268625c3602"
-    }
-}
+var webLink = $(".webLink");
 
 // hide buttons and results container
 callBtn.hide();
 webBtn.hide();
 results.hide();
+
+
+// ask user if we can get their location: Yes-> successCallBack, No -> errorCallBack
+const successCallBack = (position) => {
+    var lattitude = position.coords.latitude;
+    var longitude = position.coords.longitude;
+
+    // put in necessary info in settings 
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://developers.zomato.com/api/v2.1/search?lat=" + lattitude + "&lon=" + longitude + "&sort=cost",
+        "method": "GET",
+        "headers": {
+            "user-key": "eee323892b3fbbbde9d9b268625c3602"
+        }
+    }
+
 
 // click restaurant button to randomly generate a restaurant with specific coordinates
 randomBtn.click(function(){
@@ -57,22 +65,35 @@ randomBtn.click(function(){
         var image = $("<img>").attr("src", "https://www.maangchi.com/wp-content/uploads/2019/06/kimchistewtuna.jpg");
         imgDiv.empty().append(image);
        
+        console.log(data.restaurants[i].restaurant.thumb);
 
         // display restaurant web link
-        webBtn.click(function(){
-            webBtn.text(data.restaurants[i].restaurant.url);
-        });
+        var link = (data.restaurants[i].restaurant.url);
+        webLink.attr("href",link);
+        webLink.attr("target","_blank");
 
         // display restaurant phone number
         callBtn.click(function(){
             callBtn.text(data.restaurants[i].restaurant.phone_numbers);
-        })
+        });
+
+    });
+
+});
+
+
+}
+
+// if user does not allow browser to access location
+const errorCallBack = (error) => {
+    console.error(error);
+}
+
+// get current position of user
+navigator.geolocation.getCurrentPosition(successCallBack, errorCallBack);
 
 
 
-    })
-
-})
 
 
 
