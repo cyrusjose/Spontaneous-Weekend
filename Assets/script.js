3a8d0fb2066a420d740c5e833582f3e79b00f487
Melinda Winter <melindawinter42@gmail.com>
2020-06-19 20:29:23 -0700

    updating before pull

--- Assets/script.js
+++ Assets/script.js
@@ -1,16 +1,4 @@
-<<<<<<< HEAD
-=======
-
->>>>>>> 3f9ba483da920b7aacfbfc1a34e12bfc7d813baf
 $(document).ready(function () {
   // Required for nav bar mobile functionality
   $(".sidenav").sidenav();
-  //Required for carousel
-  $(document).ready(function () {
-    $(".carousel").carousel();
-  });
 });
-<<<<<<< HEAD
-=======
-
->>>>>>> 3f9ba483da920b7aacfbfc1a34e12bfc7d813baf
