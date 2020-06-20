b49a282aab7e2b508688a90968aeb7776afff148
kate <kan004@ucr.edu>
2020-06-20 00:07:48 -0700

    display restaurant info to card

--- Assets/script/food.js
+++ Assets/script/food.js
@@ -6,9 +6,10 @@ var longitutde = "-117.823059";
 var resName = $(".restaurant-name");
 var resPrice = $(".restaurant-price");
 var resAddress = $(".restaurant-address");
+var resCuisine = $(".restaurant-cuisine");
 var callBtn = $(".call-btn");
 var webBtn = $(".web-btn")
-
+var index = 0;
 
 var settings = {
     "async": true,
@@ -20,16 +21,23 @@ var settings = {
     }
 }
 
+callBtn.hide();
+webBtn.hide();
+
 randomBtn.click(function(){
+
+    callBtn.show();
+    webBtn.show();
 
     $.getJSON(settings, function(data){
         console.log(data);
         resName.text(data.restaurants[0].restaurant.name);
         resAddress.text("Address: " + data.restaurants[0].restaurant.location.address);
-        
+        resPrice.text("Price range: " + data.restaurants[0].restaurant.price_range);
+        resCuisine.text("Cuisine: " + data.restaurants[0].restaurant.cuisines);
 
         webBtn.click(function(){
-            webBtn.text(data.restaurants[0].restaurant.events_url);
+            webBtn.text(data.restaurants[0].restaurant.url);
         });
 
         callBtn.click(function(){
