71df8c8494dd81e39d877e9e29da55665d788874
Cyrus Jose <cyrus.jose@outlook.com>
2020-06-23 19:16:50 -0700

    Changed button text on click

--- Assets/script/movie.js
+++ Assets/script/movie.js
@@ -165,6 +165,8 @@ $(document).ready(function () {
     // Prevent default function
     event.preventDefault();
 
+    $('.generateMovie').text('Find Another Movie');
+
     //   Randomize movies
     var movieIndex = Math.floor(Math.random() * movies.length);
     //  URL for ajax call
